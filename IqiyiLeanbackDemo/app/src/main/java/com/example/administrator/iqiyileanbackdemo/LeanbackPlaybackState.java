fb73408a3b42ead00c8dd7bada560c711d792dba
huangziping <huangzp152@163.com>
2016-09-15 13:56:42 +0800

    delete

--- IqiyiLeanbackDemo/app/src/main/java/com/example/administrator/iqiyileanbackdemo/LeanbackPlaybackState.java
+++
@@ -1,8 +0,0 @@
-package com.example.administrator.iqiyileanbackdemo;
-
-/**
- * Created by Administrator on 2016/8/25.
- */
-public enum LeanbackPlaybackState  {
-    PLAYING,PAUSED,BUFFERING,IDLE
-}
